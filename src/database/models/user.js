2271c519ea346e810e4b24a56b6bc914e6d4aebd
Cristiano Sales <cristianosalesneves@gmail.com>
2022-06-24 19:35:37 -0300

    sequelize.define()

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -30,7 +30,13 @@ module.exports = (sequelize, DataTypes) => {
  */
 
 const UserSchema = (sequelize, DataTypes) => {
-
+  const UserTable = sequelize.define("User", {
+    id: DataTypes.INTEGER,
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING    
+  })
 }
 
 module.exports = UserSchema;
