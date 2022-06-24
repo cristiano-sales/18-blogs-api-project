01f28351c826de012c087ca1eedd265cedaf89e1
Cristiano Sales <cristianosalesneves@gmail.com>
2022-06-24 17:54:01 -0300

    1 - Crie migrations para as entidades User, Categories, BlogPosts, PostCategories

--- src/database/migrations/20220624201656-create-post-categories.js
+++ src/database/migrations/20220624201656-create-post-categories.js
@@ -2,26 +2,12 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('PostCategories', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
       postId: {
         type: Sequelize.INTEGER
       },
       categoryId: {
         type: Sequelize.INTEGER
       },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
     });
   },
   down: async (queryInterface, Sequelize) => {
