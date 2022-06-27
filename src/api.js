46340da26a53c6e2426470eb6e8089d69711663b
Cristiano Sales <cristianosalesneves@gmail.com>
2022-06-27 15:13:06 -0300

    errorMiddleware

--- src/api.js
+++ src/api.js
@@ -1,12 +1,13 @@
 const express = require('express');
+require('express-async-errors');
 
-// ...
+const errorMiddleware = require('./middlewares/error');
 
 const app = express();
 
 app.use(express.json());
 
-// ...
+app.use(errorMiddleware);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
---
+++ src/middlewares/error.js
@@ -0,0 +1,2 @@
+module.exports = (error, _req, res, _next) => 
+  res.status(error.status || 500).json({ message: error.message || 'Erro Interno do Servidor' });
