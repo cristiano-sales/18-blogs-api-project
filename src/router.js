4ad596acd17333b6a1829ba96b4e95fda550ed2e
Cristiano Sales <cristianosalesneves@gmail.com>
2022-06-27 16:15:55 -0300

    add required fields middleware on router

--- src/router.js
+++ src/router.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const authController = require('./controllers/authenticationController');
+const middlewares = require('./middlewares');
 
 const routes = express.Router();
 
-routes.use('/login', authController);
+routes.use('/login', middlewares.requiredFields, authController);
 
 module.exports = routes;
