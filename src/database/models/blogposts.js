f132d73cdc695d89d82abb8cc9a244dbede664fe
Cristiano Sales <cristianosalesneves@gmail.com>
2022-06-29 13:43:34 -0300

    add name on blogposts model

--- src/database/models/blogposts.js
+++ src/database/models/blogposts.js
@@ -4,16 +4,11 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class BlogPosts extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
     }
   };
   BlogPosts.init({
+    name: DataTypes.STRING,
     title: DataTypes.STRING,
     content: DataTypes.STRING,
     userId: DataTypes.INTEGER,
