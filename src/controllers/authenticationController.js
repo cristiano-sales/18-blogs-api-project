598a4b10b7c7768d4dcbcdcbf4498e19effa1103
Cristiano Sales <cristianosalesneves@gmail.com>
2022-06-29 13:28:07 -0300

    modified authentication controller

--- src/controllers/authenticationController.js
+++ src/controllers/authenticationController.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const authService = require('../services/authenticationService');
 
 const authRouter = express.Router();
 
-authRouter.post('/', (req, res) => {
-  const newUser = req.body;
-  return res.status(201).json(newUser);
+authRouter.post('/', async (req, res) => {
+  const newUserToken = await authService.authenticator(req.body);
+  return res.status(200).json(newUserToken);
 });
 
 module.exports = authRouter;
