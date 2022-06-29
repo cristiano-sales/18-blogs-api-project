9c4c44609250e1ff874952eece5cffe6db9e046c
Cristiano Sales <cristianosalesneves@gmail.com>
2022-06-29 13:30:37 -0300

    jwt

--- src/jwt.js
+++ src/jwt.js
@@ -2,8 +2,13 @@ const jwt = require('jsonwebtoken');
 
 const TOKEN_SECRET = process.env.TOKEN_SECRET || 'secretJWT';
 
+const jwtConfig = {
+  expiresIn: '45m',
+  algorithm: 'HS256',
+};
+
 const generateToken = ({ displayName, email, image }) =>
-  jwt.sign({ displayName, email, image }, TOKEN_SECRET);
+  jwt.sign({ displayName, email, image }, TOKEN_SECRET, jwtConfig);
 
 module.exports = {
   generateToken,
