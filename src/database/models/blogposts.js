de544d5c198bbb31eb2da0446bfadb30836913b3
Cristiano Sales <cristianosalesneves@gmail.com>
2022-06-29 13:53:56 -0300

    BlogPostsSchema

--- src/database/models/blogposts.js
+++ src/database/models/blogposts.js
@@ -1,22 +1,25 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class BlogPosts extends Model {
-    static associate(models) {
-    }
-  };
-  BlogPosts.init({
+const BlogPostsSchema = (sequelize, DataTypes) => {
+  const BlogPostsTable = sequelize.define("BlogPost", {
     name: DataTypes.STRING,
     title: DataTypes.STRING,
     content: DataTypes.STRING,
     userId: DataTypes.INTEGER,
     published: DataTypes.DATE,
     updated: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'BlogPosts',
   });
-  return BlogPosts;
+
+  BlogPostsTable.associate = ({ User }) => {
+    BlogPostsTable.belongsTo(User, { as: 'userPost', foreignKey: 'userId' })
+  }
+  return BlogPostsTable;
 };
+
+module.exports = BlogPostsSchema;
+
+
+/**
+* belongsTo, pertence a
+* A associação A.belongsTo(B) significa que existe
+* um relacionamento Um-para-Um entre Ae B,
+* com a chave estrangeira sendo definida no modelo de origem (A)
+*/
