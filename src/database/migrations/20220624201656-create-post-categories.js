d49eb8beb862b4cdf433ffffc3eb25154152a8c9
Cristiano Sales <cristianosalesneves@gmail.com>
2022-06-29 13:28:54 -0300

    modified migration create post categories

--- src/database/migrations/20220624201656-create-post-categories.js
+++ src/database/migrations/20220624201656-create-post-categories.js
@@ -1,16 +1,31 @@
 'use strict';
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('PostCategories', {
       postId: {
-        type: Sequelize.INTEGER
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'BlogPosts',
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+        primaryKey: true
       },
       categoryId: {
-        type: Sequelize.INTEGER
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+        primaryKey: true
       },
     });
   },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('PostCategories');
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('PostsCategories');
   }
 };
