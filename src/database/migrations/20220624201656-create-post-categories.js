ec08c89b52bf048447f824e6f409893e1b2fce30
Cristiano Sales <cristianosalesneves@gmail.com>
2022-07-01 19:17:25 -0300

    create post categories migration

--- src/database/migrations/20220624201656-create-post-categories.js
+++ src/database/migrations/20220624201656-create-post-categories.js
@@ -25,7 +25,7 @@ module.exports = {
       },
     });
   },
-  down: async (queryInterface, _Sequelize) => {
+  down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('PostsCategories');
   }
 };
