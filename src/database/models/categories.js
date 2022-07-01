87cf34c2cf076d74615c42bebf64cf6884abc3a1
Cristiano Sales <cristianosalesneves@gmail.com>
2022-07-01 19:22:23 -0300

    update categories schema

--- src/database/models/categories.js
+++ src/database/models/categories.js
@@ -1,6 +1,13 @@
 const CategoriesSchema = (sequelize, DataTypes) => {
-  const CategoryTable = sequelize.define("Categories", {
+  const CategoryTable = sequelize.define("Category", {
+    id: {
+       type: DataTypes.INTEGER,
+       primaryKey: true,
+       autoIncrement: true,
+    },
     name: DataTypes.STRING,
+  }, {
+    timestamps: false
   });
   return CategoryTable;
 };
