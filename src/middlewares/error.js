b00666cd146f0c17f58543c70b45b7f11e962fef
Cristiano Sales <cristianosalesneves@gmail.com>
2022-07-01 19:29:41 -0300

    update error middleware

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,2 +1,2 @@
-module.exports = (error, _req, res, _next) => 
-  res.status(error.status || 500).json({ message: error.message || 'Erro Interno do Servidor' });
+module.exports = (error, req, res, _next) => 
+  res.status(error.status || 500).json({ message: error.message || 'Erro inesperado' }); 
