26db66f8c2874c5d7be9c6d41cb792ed345b61a0
Cristiano Sales <cristianosalesneves@gmail.com>
2022-07-01 19:20:53 -0300

    update blog post schema

--- src/database/models/blogposts.js
+++ src/database/models/blogposts.js
@@ -1,21 +1,24 @@
 const BlogPostsSchema = (sequelize, DataTypes) => {
   const BlogPostsTable = sequelize.define("BlogPost", {
-    name: DataTypes.STRING,
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+   },
     title: DataTypes.STRING,
     content: DataTypes.STRING,
     userId: DataTypes.INTEGER,
     published: DataTypes.DATE,
     updated: DataTypes.DATE
-  });
+  }, { timestamps: false });
 
   BlogPostsTable.associate = ({ User }) => {
-    BlogPostsTable.belongsTo(User, { as: 'userPost', foreignKey: 'userId' })
+    BlogPostsTable.belongsTo(User, { as: 'user', foreignKey: 'userId' })
   }
   return BlogPostsTable;
 };
 
 module.exports = BlogPostsSchema;
-
 
 /**
 * belongsTo, pertence a
