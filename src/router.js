b2bec82451c2b68b039adc5ad227db4bc7a6ba2d
Cristiano Sales <cristianosalesneves@gmail.com>
2022-07-01 19:46:03 -0300

    routers

--- src/router.js
+++ src/router.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const authController = require('./controllers/authenticationController');
+const userController = require('./controllers/userController');
+const categoryController = require('./controllers/categoryController');
+const blogPostController = require('./controllers/blogPostController');
+
 const middlewares = require('./middlewares');
 
 const routes = express.Router();
 
-routes.use('/login', middlewares.requiredFields, authController);
+routes.use('/login', authController);
+routes.use('/user', userController);
+routes.use('/categories', middlewares.authenticate, categoryController);
+routes.use('/post', middlewares.authenticate, blogPostController);
 
 module.exports = routes;
