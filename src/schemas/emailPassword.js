e018e9e8b2e6a75a109071a3ea89f8f1a38e07ef
Cristiano Sales <cristianosalesneves@gmail.com>
2022-07-01 19:47:01 -0300

    schemas

--- src/schemas/emailPassword.js
+++ src/schemas/emailPassword.js
@@ -1,9 +1,11 @@
 const Joi = require('joi');
 // linguagem de descrição de schema e validação de dados
 
-const emailPassword = Joi.object({
+const validateUserData = Joi.object({
+  displayName: Joi.string().min(8).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
+  image: Joi.string().required(),
 });
 
-module.exports = emailPassword; 
+module.exports = validateUserData;
