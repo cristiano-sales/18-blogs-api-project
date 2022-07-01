235334c977426de7054f2667d5bbb6b6c1ba6888
Cristiano Sales <cristianosalesneves@gmail.com>
2022-07-01 19:26:30 -0300

    update user schema

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,16 +1,17 @@
 const UserSchema = (sequelize, DataTypes) => {
   const UserTable = sequelize.define("User", {
-    id: DataTypes.INTEGER,
+    // id: DataTypes.INTEGER,
+    id: { type: DataTypes.INTEGER, primaryKey: true },
     displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
-    image: DataTypes.STRING    
+    image: DataTypes.STRING,
   });
   UserTable.associate = ({ BlogPost }) => {
     UserTable.hasMany(BlogPost, { as: 'posts', foreignKey: 'userId' })
   }
   return UserTable;
-};
+}
 
 module.exports = UserSchema;
 
