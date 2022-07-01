da6923ee5307588526a3cc5ed0e75eaf02854383
Cristiano Sales <cristianosalesneves@gmail.com>
2022-07-01 18:27:37 -0300

    ajustes in authentication service

--- src/controllers/authenticationController.js
+++ src/controllers/authenticationController.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const authService = require('../services/authenticationService');
+const middlewares = require('../middlewares');
 
 const authRouter = express.Router();
 
-authRouter.post('/', async (req, res) => {
-  const newUserToken = await authService.authenticator(req.body);
-  return res.status(200).json(newUserToken);
+authRouter.post('/', middlewares.isNotEmpty, async (req, res) => {
+  const token = await authService.authenticate(req.body);
+  return res.status(200).json(token);
 });
 
 module.exports = authRouter;
